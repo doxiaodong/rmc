c4ba5cb5611a1a75514ffc212a890dcf65e6e775
duxiaodong <duxiaodong@darlin.me>
2019-05-17 16:30:17 +0800

    init

---
+++ .umirc.library.ts
@@ -0,0 +1,6 @@
+export default {
+  esm: { type: 'babel' },
+  cjs: { type: 'babel' },
+  umd: {},
+  runtimeHelpers: true,
+};
---
+++ packages/rmc-pull-to-refresh/src/index.ts
@@ -0,0 +1,8 @@
+import PullToRefresh from './PullToRefresh';
+
+export default PullToRefresh;
+
+async function a() {
+  await 1;
+  return 2;
+}
